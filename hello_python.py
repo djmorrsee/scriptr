548a56b0a04d66f4682bb8299ea3ca4cf35342c4
Daniel Morrissey <djmorrsee@gmail.com>
2015-04-15 16:15:06 -0600

    Python syntax and demo

---
+++ hello_python.py
@@ -0,0 +1,9 @@
+# This file is here to help you learn how to run python
+import sys
+
+def main():
+	print 'Hello, python!'
+	
+# Run main function if this script was launched directly from command line, otherwise do nothing
+if __name__ == '__main__':
+	main()
