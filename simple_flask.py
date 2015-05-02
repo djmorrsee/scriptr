c30bfc32890ad8ff779cf162ce520a0f326a8d16
Daniel Morrissey <djmorrsee@gmail.com>
2015-05-01 22:36:07 -0600

    Flask examples

--- simple_flask.py
+++ simple_flask.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello():
 	return "Hello Brandon, your penis is small"
 	
+@app.route("/path/")
+def path():
+	return "Another URL"
+	
+@app.route("/user/<username>") 
+def user():
+	return "Your name is " + str(username)
+	
 if __name__ == "__main__":
 	app.run(host="0.0.0.0")
