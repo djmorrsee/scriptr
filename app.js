c78a165ba704274d2a0e0c39d4a16d2083097f77
Daniel Morrissey <djmorrsee@gmail.com>
2015-05-14 20:45:15 -0600

    Server now serves a boilerplate frontend.

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-var websocketserver = require('ws').Server
+var fs = require ('fs');
+var path = require('path');
+
+var websocketserver = require('ws').Server;
+
 var express = require('express');
 var app = express();
+
+app.use(express.static('public'));
 
 var doc_server = new websocketserver({port:3555});
 doc_server.on('connection', function (socket) {
 	socket.on('message', function (message) {
-		console.log(message)
+		console.log(message);
 	});
 });
 
 app.get('/', function (req, res) {
-	res.end()
+	res.sendfile('index.html');
+	res.end();
 });
 
 var server = app.listen(3000, function () {
