84df97a906aba5b245083a35ed2cd7dd8bd8a3e7
Daniel Morrissey <djmorrsee@gmail.com>
2015-05-15 17:21:55 -0600

    Document Manager and tests

---
+++ src/docmanager.js
@@ -0,0 +1,46 @@
+var exports = module.exports = {};
+
+function DocumentBuffer () {
+	this.buffer = [];
+}
+
+DocumentBuffer.prototype.HashBuffer = function () {
+	return require('crypto').createHash('md5').update(this.buffer.join('')).digest('hex');
+};
+
+DocumentBuffer.prototype.AddChars = function (pos, chars) {
+	var buff = this.buffer
+	var vc = this._ValidChar
+	
+	var valid = chars.split('').map(function (val) {
+		return vc(val)
+	}).reduce(function (l, r) {
+		return l || r;
+	})
+	
+	if(!valid) {
+		throw new Error('Invalid character');
+	}
+	
+	// Normalize to avoid negative range issues
+	pos = pos < 0 ? 0 : pos;
+	
+	chars.split('').forEach(function(char) {
+		buff.splice(pos++, 0, char);
+	});
+};
+
+DocumentBuffer.prototype.RemoveChars = function (start, count) {
+	
+	// Normalize for negative indeces
+	start = start < 0 ? 0 : start;
+	
+	this.buffer = (this.buffer.slice(0, start).join('') + this.buffer.slice(start+count).join('')).split('');
+};
+
+DocumentBuffer.prototype._ValidChar = function (char) {
+	var val = char.charCodeAt(0);
+	return 32 <= val && val <= 126;
+};
+
+module.exports = DocumentBuffer
---
+++ test/test_docmanager.js
@@ -0,0 +1,149 @@
+var assert = require('assert');
+var path = require('path')
+
+var DocumentBuffer = require('../lib/docmanager.js');
+
+
+describe('DocumentBuffer', function () {
+	
+	// Initialization Test
+	it('should have an empty buffer at creation', function () {
+		var db = new DocumentBuffer();
+		assert.equal(db.buffer.length, 0);
+	});
+	
+	// Valid Buffer Contents Test
+	describe('#_ValidChar', function () {
+		it('should return true when given a printable character', function () {
+			// Test ASCII code 32-126
+			var db = new DocumentBuffer();
+			
+			for (var i = 32; i <= 126; ++i) {
+				var ch = String.fromCharCode(i);
+				assert.equal(db._ValidChar(ch), 1);
+			}
+		});
+		
+		it('should return false when given a non-printable character', function () {
+			// Test ASCII code 0-31, 127-...
+			var db = new DocumentBuffer();
+			
+			for (var i = 0; i < 32; ++i) {
+				var ch = String.fromCharCode(i);
+				assert.equal(db._ValidChar(ch), 0);
+			}
+
+		});
+		
+	});
+	
+	// Insertion Tests
+	describe('#AddChars', function () {
+		it('should allow insertion of a char at any index', function () {
+			var db = new DocumentBuffer();
+			
+			db.AddChars(0, 'a');
+			assert.deepEqual(db.buffer, ['a']);
+			
+			db.AddChars(0, 'b');
+			assert.deepEqual(db.buffer, ['b', 'a']);
+			
+			db.AddChars(1, 'c');
+			assert.deepEqual(db.buffer, ['b', 'c', 'a']);
+			
+			db.AddChars(3, 'd');
+			assert.deepEqual(db.buffer, ['b', 'c', 'a', 'd']);
+		});
+		
+		it('should throw an error if trying to insert non-printable characters', function () {
+			var db = new DocumentBuffer();
+			assert.throws(function () { db.AddChars(0, String.fromCharCode(1)) }, Error);
+		});
+		
+		it('should insert at the end for indexes > length', function () {
+			var db = new DocumentBuffer();
+			
+			db.AddChars(0, 'a');
+			assert.deepEqual(db.buffer, ['a']);
+			
+			db.AddChars(1200, 'b');
+			assert.deepEqual(db.buffer, ['a', 'b']);
+		});
+		
+		it('should insert at the beginning for indexes < 0', function () {
+			var db = new DocumentBuffer();
+			
+			db.AddChars(0, 'a');
+			assert.deepEqual(db.buffer, ['a']);
+			
+			db.AddChars(-1200, 'b');
+			assert.deepEqual(db.buffer, ['b', 'a']);
+			
+		});
+		
+		it('should allow the insertion of a range of characters', function () {
+			var db = new DocumentBuffer();
+			
+			db.AddChars(0, 'a');
+			assert.deepEqual(db.buffer, ['a']);
+			
+			db.AddChars(-500, 'bad');
+			assert.deepEqual(db.buffer, ['b', 'a', 'd', 'a']);
+			
+			db.AddChars(2, 'xyz');
+			assert.deepEqual(db.buffer, ['b', 'a', 'x', 'y', 'z', 'd', 'a']);
+			
+		});	
+	});
+	
+	describe('#RemoveChars', function () {
+		it('should remove a chunk of characters from the buffer, given by a starting index and a count', function () {
+			var db = new DocumentBuffer();
+			
+			db.AddChars(0, 'abcd');
+			db.RemoveChars(1, 1);
+			
+			assert.deepEqual(db.buffer, ['a', 'c', 'd']);
+			
+			db.RemoveChars(0, 1000);
+			assert.deepEqual(db.buffer, []);
+		});
+		
+		it('should do nothing if remove count is 0', function () {
+			var db = new DocumentBuffer();
+			
+			db.AddChars(0, 'abc');
+			db.RemoveChars(1, 0);
+			
+			assert.deepEqual(db.buffer, ['a', 'b', 'c']);
+		});
+		
+		it('should normalize indeces out of range of the buffer', function () {
+			var db = new DocumentBuffer();
+			
+			db.AddChars(0, 'abc');			
+			assert.deepEqual(db.buffer, ['a', 'b', 'c']);
+			
+			db.RemoveChars(-500, 2);
+			assert.deepEqual(db.buffer, ['c']);
+		});
+	});
+	
+	describe('#HashBuffer', function () {
+		it('should hash the entire buffer and return it, using md5', function () {
+			var db = new DocumentBuffer();
+			assert.equal(db.HashBuffer(), db.HashBuffer());
+		});
+		
+		it('should always have a different hash for different buffers', function () {
+			// This is a limit of md5, in practice we will never have a collision
+			var db = new DocumentBuffer();
+			
+			var old_hash = db.HashBuffer();
+			db.AddChars(0, 'abcd');
+			
+			assert.notEqual(old_hash, db.HashBuffer());
+			
+		});
+	});
+});
