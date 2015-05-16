e9594780f84f008edebad42a7e536ba59fe5b11e
Brandon <branvail@hotmail.com>
2015-05-15 21:46:58 -0600

    separated code and html, made code editor the home page

---
+++ public/js/scriptrMain.js
@@ -0,0 +1,82 @@
+	var oldVal = $(scriptBox).val();
+	$(scriptBox).on('change keyup paste', function() {
+		var newVal = $(scriptBox).val();
+		if (newVal == oldVal) {
+			return;
+		}
+		//console.log(newVal);
+		var oldString = oldVal.split('');
+		var newString = newVal.split('');
+
+		var isDelete = false;
+		
+		var shortString = oldString;
+		var longString = newString;
+
+		if (oldString.length > newString.length) {
+			isDelete = true
+			shortString = newString;
+			longString = oldString;
+		}
+
+		var i = 0;
+		for (i = 0; i < shortString.length; i++) {
+			if (longString[i] != shortString[i]) {
+				break;
+			}
+		}
+		var changeStart = i;
+		var changeEnd = 0;
+		for (var x = 0; x < longString.length; x++) {
+			if (longString[longString.length-x] != shortString[shortString.length-x]) {
+				changeEnd = (longString.length-x)+1;
+				break;
+			}
+		}
+		
+		if (changeStart > changeEnd) {
+			var temp = changeStart;
+			changeStart = changeEnd;
+			changeEnd = temp;
+		}
+		if (changeEnd == changeStart)
+			changeEnd++;
+			
+		var finalArray = longString.slice(changeStart, changeEnd);
+		var changed = finalArray.join('');
+		if (finalArray[0] == " " && finalArray[finalArray.length-1] != " " && changed.charAt(0) != " ")
+			changed = " " + changed;
+		else if (finalArray[finalArray.length-1] == " " && finalArray[0] != " " && changed.charAt(finalArray.length-1) != " ")
+			changed = changed + " ";
+			
+		debug (changed, changeStart, changeEnd, isDelete);
+			
+		receiveChange(changed, changeStart, changeEnd, isDelete);
+
+		oldVal = newVal;
+		
+	});
+function debug (text, start, end, isDelete) {
+		if (!isDelete) {
+			//document.getElementById("scriptBox2").value = "added: " + changed;
+			console.log("added:" + text);
+		}
+		else {
+			//document.getElementById("scriptBox2").value = "deleted: " + changed;
+			console.log("deleted:" + text);
+		}
+
+		//console.log(changeStart);
+		//console.log(changeEnd);
+}
+	
+function receiveChange(changedString, start, end, isDelete) {
+    var current = $(scriptBox2).val();
+    var finalText = "";
+    if (!isDelete) {
+        finalText = current.slice(0, start) + changedString + current.slice(start);
+    } else {
+        finalText = current.slice(0, start) + current.slice(end);
+    }
+    document.getElementById("scriptBox2").value = finalText;
+}
