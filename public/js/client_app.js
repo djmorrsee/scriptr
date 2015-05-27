1a41bf0c6b716d0a986478fb3102dfc524358369
Daniel Morrissey <djmorrsee@gmail.com>
2015-05-26 20:06:04 -0600

    Added save alert, theres probably a nicer js alert we could find.

--- public/js/client_app.js
+++ public/js/client_app.js
@@ -1,6 +1,6 @@
 var Scriptr = function () {
-	this.socket = new ScriptrSocket('127.0.0.1', DOCUMENT_PORT);
-	// this.socket = new ScriptrSocket('djmorrsee.me', DOCUMENT_PORT);
+	// this.socket = new ScriptrSocket('127.0.0.1', DOCUMENT_PORT);
+	this.socket = new ScriptrSocket('djmorrsee.me', DOCUMENT_PORT);
 
 	this.doc
 	this.chat
@@ -18,6 +18,7 @@ $(document).ready (function () {
 
 	save_button.on('click', function() {
 		scriptr.socket.SendSaveMessage();
+		alert('File saved at djmorrsee.me/' + DOCUMENT_PORT)
 	});
 	new_button.on('click', function () {
 		window.location.href = "/"
